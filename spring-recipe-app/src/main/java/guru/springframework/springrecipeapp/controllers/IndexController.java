84f9e7aaf18e9af40729c2198e1e6750c7f98404
KajcsaRenataGabriela <67558003+KajcsaRenataGabriela@users.noreply.github.com>
2021-09-24 13:32:03 +0300

    Refactored POJOs Added Bootstrap

--- spring-recipe-app/src/main/java/guru/springframework/springrecipeapp/controllers/IndexController.java
+++ spring-recipe-app/src/main/java/guru/springframework/springrecipeapp/controllers/IndexController.java
@@ -1,33 +1,23 @@
 package guru.springframework.springrecipeapp.controllers;
 
-import guru.springframework.springrecipeapp.domain.Category;
-import guru.springframework.springrecipeapp.domain.UnitOfMeasurement;
-import guru.springframework.springrecipeapp.repositories.CategoryRepository;
-import guru.springframework.springrecipeapp.repositories.UnitOfMeasurementRepository;
+import guru.springframework.springrecipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasurementRepository unitOfMeasurementRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasurementRepository unitOfMeasurementRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasurementRepository = unitOfMeasurementRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasurement> unitOfMeasureOptional = unitOfMeasurementRepository.findByDescription("Teaspoon");
-
-//        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-//        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
--- spring-recipe-app/src/main/java/guru/springframework/springrecipeapp/domain/Recipe.java
+++ spring-recipe-app/src/main/java/guru/springframework/springrecipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.springrecipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,16 +17,18 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
-    private String direction;
 
-    @Enumerated(value = EnumType.STRING)
-    private Difficulty difficulty;
+    @Lob
+    private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -34,23 +37,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
-    }
-
-    public Set<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(Set<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -108,12 +95,12 @@ public class Recipe {
         this.url = url;
     }
 
-    public String getDirection() {
-        return direction;
+    public String getDirections() {
+        return directions;
     }
 
-    public void setDirection(String direction) {
-        this.direction = direction;
+    public void setDirections(String directions) {
+        this.directions = directions;
     }
 
     public Byte[] getImage() {
@@ -132,11 +119,27 @@ public class Recipe {
         this.notes = notes;
     }
 
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     public Difficulty getDifficulty() {
         return difficulty;
     }
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
